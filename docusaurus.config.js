c104d536c43feb905f78cf23c6a5f768870f6f0b
Pribal <oscar.giraudoux@gmail.com>
2024-04-15 19:18:59 +0200

    gh-page config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: 'logos/logo_noir.png',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://pribal.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/blog/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -74,7 +74,7 @@ const config = {
             position: 'left',
             label: 'Walkthroughts',
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
+          {to: 'blog', label: 'Blog', position: 'left'},
         ],
       },
       footer: {
--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/blog">
+            to="blog">
               Articles 📚
           </Link>
           <Link
